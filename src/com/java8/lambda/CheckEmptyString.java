6c1479f968189314b5de251b8927a0bcc47f0ab2
Deepak <deepak.281295@gmail.com>
2024-03-30 12:43:46 +0530

    Lambda - check empty string

---
+++ src/com/java8/lambda/CheckEmptyString.java
@@ -0,0 +1,17 @@
+package com.java8.lambda;
+
+import java.util.function.Predicate;
+
+public class CheckEmptyString {
+    public static void main(String[] args) {
+        Predicate<String> isEmptyString = str -> str.isEmpty();
+
+        String str1 = "";
+        String str2 = "Java lambda expression!";
+
+        System.out.println("String 1: " + str1);
+        System.out.println("String 1 is empty: " + isEmptyString.test(str1));
+        System.out.println("\nString 2:" + str2);
+        System.out.println("String 2 is empty: " + isEmptyString.test(str2));
+    }
+}
